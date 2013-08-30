00122b70d14c16b1ca60cd7cd8591d6211b52b27
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2013-08-30 14:55:16 +0200

    deactivated unneeded modules for debugging

--- build.py
+++ build.py
@@ -1,11 +1,11 @@
 from pybuilder.core import use_plugin, init, Author
 
 use_plugin("python.install_dependencies")
-use_plugin("copy_resources")
+#use_plugin("copy_resources")
 use_plugin("python.core")
-use_plugin("python.unittest")
+#use_plugin("python.unittest")
 #use_plugin("python.coverage")
-use_plugin("python.pylint")
+#use_plugin("python.pylint")
 use_plugin("python.distutils")
 
 authors = [Author('Marco Hoyer', 'marco.hoyer@immobilienscout24.de')]
