cd88f11f5be12fb9af8c0b604d86f2c9da0530fb
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2013-09-11 13:23:11 +0200

    fixed bug generating invalid graphite metrics if icinga perfdata contains spaces

--- src/main/python/CarbonFormatter.py
+++ src/main/python/CarbonFormatter.py
@@ -8,6 +8,9 @@ class CarbonFormatter():
 		logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
 		self.logger = logging.getLogger('CarbonFormatter')
 
+	def convert_string(self, string):
+		return "_".join( string.split() ).lower()
+
 	def format(self,metric_prefix, parsed_data):
 		if parsed_data is None:
 			self.logger.warn('Received invalid data')
@@ -16,5 +19,5 @@ class CarbonFormatter():
 		for metric in parsed_data['metrics']:
 			typ = parsed_data['hostname']
 			typ = typ[3:-2]
-			formatted_data.append('%s.%s.%s.%s.%s %s %s\n' % (typ.lower(), parsed_data['hostname'].lower(), metric_prefix.lower(), parsed_data['servicename'].lower(), metric['name'].lower(), metric['value'], parsed_data['timestamp']))
+			formatted_data.append('%s.%s.%s.%s.%s %s %s\n' % (self.convert_string(typ), self.convert_string(parsed_data['hostname']), self.convert_string(metric_prefix), self.convert_string(parsed_data['servicename']), self.convert_string(metric['name']), metric['value'], parsed_data['timestamp']))
 		return formatted_data
