90ac01bb5d59d9a533f8058627bf2593ea8c7adf
Valentin Haenel <valentin.haenel@gmx.de>
2013-11-08 10:51:10 +0100

    reduce test code boilerplate for the IcingaParser_tests.py

--- src/unittest/python/IcingaParser_tests.py
+++ src/unittest/python/IcingaParser_tests.py
@@ -3,20 +3,19 @@ from metricd.IcingaParser import IcingaParser
 
 class IcingaParserTest(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.parser = IcingaParser()
+        cls.result = cls.parser.parse('tuvdbs50|Ping|rta=0.888000ms;3000.000000;5000.000000;0.000000 pl=0%;80;100;0|1364909110')
+
     def test_parses_hostname(self):
-        parser = IcingaParser()
-        result = parser.parse('tuvdbs50|Ping|rta=0.888000ms;3000.000000;5000.000000;0.000000 pl=0%;80;100;0|1364909110')
-        self.assertEquals(result.get('hostname'), 'tuvdbs50')
+        self.assertEquals(self.result.get('hostname'), 'tuvdbs50')
 
     def test_parses_servicename(self):
-        parser = IcingaParser()
-        result = parser.parse('tuvdbs50|Ping|rta=0.888000ms;3000.000000;5000.000000;0.000000 pl=0%;80;100;0|1364909110')
-        self.assertEquals(result.get('servicename'), 'ping')
+        self.assertEquals(self.result.get('servicename'), 'ping')
 
     def test_parses_metrics(self):
-        parser = IcingaParser()
-        result = parser.parse('tuvdbs50|Ping|rta=0.888000ms;3000.000000;5000.000000;0.000000 pl=0%;80;100;0|1364909110')
-        metrics = result.get('metrics')
+        metrics = self.result.get('metrics')
         self.assertEquals(len(metrics), 2)
         self.assertEquals(metrics[0].get('name'), 'rta')
         self.assertEquals(metrics[0].get('value'), '0.888000')
@@ -24,13 +23,10 @@ class IcingaParserTest(unittest.TestCase):
         self.assertEquals(metrics[1].get('value'), '0')
 
     def test_parses_timestamp(self):
-        parser = IcingaParser()
-        result = parser.parse('tuvdbs50|Ping|rta=0.888000ms;3000.000000;5000.000000;0.000000 pl=0%;80;100;0|1364909110')
-        self.assertEquals(result.get('timestamp'), '1364909110')
+        self.assertEquals(self.result.get('timestamp'), '1364909110')
 
     def test_parses_empty_line(self):
-        parser = IcingaParser()
-        result = parser.parse('')
+        result = self.parser.parse('')
         self.assertTrue(result is None)
 
 if __name__ == '__main__':
