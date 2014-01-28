95a4ebe93baed3d1d31cadd92fb1ebf0f034cdf8
mhoyer <marco_hoyer@gmx.de>
2014-01-28 10:30:00 +0100

    add check for hostname validity to ensure type prefixes only being created if there is a valid hostname to extract it from

--- src/main/python/metricd/CarbonFormatter.py
+++ src/main/python/metricd/CarbonFormatter.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python
 
 import logging
+import re
+
 
 class CarbonFormatter():
 
-	def __init__(self):
-		self.logger = logging.getLogger(__name__)
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.default_hostname_pattern = re.compile(r"[A-Za-z]{6}[0-9]{2}")
 
-	def convert_string(self, string):
-		return "_".join( string.split() ).lower()
+    def convert_string(self, string):
+        return "_".join(string.split()).lower()
 
-	def format(self,metric_prefix, parsed_data):
-		if parsed_data is None:
-			return None
-		formatted_data = []
-		for metric in parsed_data['metrics']:
-			self.logger.debug("Writing metric to graphite: " + str(metric))
-			typ = parsed_data['hostname']
-			typ = typ[3:-2]
-			formatted_data.append('%s.%s.%s.%s.%s %s %s\n' % (self.convert_string(typ), self.convert_string(parsed_data['hostname']), self.convert_string(metric_prefix), self.convert_string(parsed_data['servicename']), self.convert_string(metric['name']), metric['value'], parsed_data['timestamp']))
-		return formatted_data
+
+    def get_type_from_hostname(self, hostname):
+        if self.default_hostname_pattern.match(hostname):
+            return hostname[3:-2]
+        else:
+            return ""
+
+    def format(self, metric_prefix, parsed_data):
+        if parsed_data is None:
+            return None
+        formatted_data = []
+        for metric in parsed_data['metrics']:
+            self.logger.debug("Writing metric to graphite: " + str(metric))
+            typ = self.get_type_from_hostname(parsed_data['hostname'])
+            metric_string = '%s.%s.%s.%s.%s %s %s\n' % (
+                self.convert_string(typ), self.convert_string(parsed_data['hostname']), self.convert_string(metric_prefix),
+                self.convert_string(parsed_data['servicename']), self.convert_string(metric['name']), metric['value'],
+                parsed_data['timestamp'])
+            formatted_data.append(metric_string.strip('.'))
+        return formatted_data
--- src/unittest/python/CarbonFormatter_tests.py
+++ src/unittest/python/CarbonFormatter_tests.py
@@ -14,6 +14,11 @@ class CarbonFormatterTest(unittest.TestCase):
         self.assertEquals(formatter.convert_string(" a b c "), "a_b_c")
         self.assertEquals(formatter.convert_string(" "), "")
 
+    def test_get_type_from_hostname(self):
+        formatter = CarbonFormatter()
+        self.assertEqual(formatter.get_type_from_hostname("tuvdbs50"),"dbs")
+        self.assertEqual(formatter.get_type_from_hostname("my-super-uplink"),'')
+
     def test_format_carbon_lines(self):
         formatter = CarbonFormatter()
         metric = {'metrics': [{'name': 'rta', 'value': '0.888000'},
@@ -41,6 +46,20 @@ class CarbonFormatterTest(unittest.TestCase):
                 'dbs.tuvdbs50.icinga.system_ping.rta 0.888000 1364909110\n')
         self.assertEquals(result[1],
                 'dbs.tuvdbs50.icinga.system_ping.metric_with_spaces 0 1364909110\n')
+
+    def test_format_metrics_with_special_hostname(self):
+        formatter = CarbonFormatter()
+        metric = {'metrics': [{'name': 'rta', 'value': '0.888000'},
+                              {'name': 'metric with spaces', 'value': '0'}],
+                  'timestamp': '1364909110',
+                  'hostname': 'my-super-uplink',
+                  'servicename': 'system ping'}
+        result = formatter.format('icinga', metric)
+        self.assertEquals(len(result), 2)
+        self.assertEquals(result[0],
+                'my-super-uplink.icinga.system_ping.rta 0.888000 1364909110\n')
+        self.assertEquals(result[1],
+                'my-super-uplink.icinga.system_ping.metric_with_spaces 0 1364909110\n')
 
 if __name__ == '__main__':
     unittest.main()
